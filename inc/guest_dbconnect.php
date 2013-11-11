01222b918c9fba0255add71fa71aae5563afdacb
Michael Davidson <mikeyed101@gmail.com>
2013-11-11 09:07:57 -0500

    Update guest_dbconnect.php

--- inc/guest_dbconnect.php
+++ inc/guest_dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 	try {
-		$db = new PDO("mysql:host=localhost;dbname=Guests", "root", "Koala812");
+		$db = new PDO("mysql:host=localhost;dbname=Guests", "root", "root");
 		$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$db->exec("SET NAMES 'utf8'");
 	}  catch (Exception $e) {
