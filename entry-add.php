ef46d00c6c3ca62bfe3c0a3ceca180761874d8d3
Michael Davidson <mikeyed101@gmail.com>
2013-11-18 16:32:55 -0500

    Update entry-add.php

--- entry-add.php
+++ entry-add.php
@@ -2,12 +2,17 @@
 //Connect to Guest database
 require("inc/guest_record.php");
 
-$err_message = validate_entry($_POST["name"], $_POST["email"], $_POST["comment"]);
+$name = clean_val($_POST["name"]);
+$email = clean_val($_POST["email"]);
+$comment = clean_val($_POST["comment"]);
+
+$err_message = validate_entry($name, $email, $comment);
 
 if(!isset($err_message)) {
 		
 	//Define posted variables by user
-	add_entry( $_POST["name"], $_POST["email"], $_POST["comment"]);
+	//add_entry( $_POST["name"], $_POST["email"], $_POST["comment"]);
+	add_entry($name, $email, $comment);
 
 	//Return to thank you page
 	header("Location: visitor-thanks.php");
