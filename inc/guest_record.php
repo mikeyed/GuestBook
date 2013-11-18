861b75389389ab6bfb9363eb4e8643bc90aac0dc
Michael Davidson <mikeyed101@gmail.com>
2013-11-18 12:41:43 -0500

    Update guest_record.php

--- inc/guest_record.php
+++ inc/guest_record.php
@@ -13,17 +13,30 @@ function get_list_view_html($entry) {
  	return $output;
 }
 
+
+function clean_val($value) {
+	$value_clean = trim($value);
+	$value_clean = stripslashes($value);
+	$value_clean = htmlspecialchars($value);
+	
+	return $value_clean;
+}
+	
 /* add a guest entry to the database */
 function add_entry($name, $email, $comment) {
 	require("inc/guest_dbconnect.php");
+	
+	$name_clean = clean_val($name);
+	$email_clean = clean_val($email);
+	$comment_clean = clean_val($comment);
 	
 	try { 
 		//State table and variables being inserted
 		$stmt = $db->prepare("INSERT INTO GuestBook (name, email, comment) VALUES (:name, :email, :comment)");
 		//Bind those variables inserted with the ones posted 
-		$stmt->bindParam(':name', $name);
-		$stmt->bindParam(':email', $email);
-		$stmt->bindParam(':comment', $comment);
+		$stmt->bindParam(':name', $name_clean);
+		$stmt->bindParam(':email', $email_clean);
+		$stmt->bindParam(':comment', $comment_clean);
 		//Execute the prepared statement
 		$stmt->execute();
 	} catch (Exception $e) {
@@ -31,6 +44,33 @@ function add_entry($name, $email, $comment) {
 			exit;	
 	}
 }
+
+function validate_entry($name, $email, $comment) {
+	// define variables and set to empty values
+	$name = $email = $comment = "";
+			
+
+	/* reviews form submission for errors, omissions, or bots */
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		$name =  trim($_POST["name"]);
+		$email = trim($_POST["email"]);
+		$comment = trim($_POST["comment"]);
+		
+		if($name == "" OR $email == "" OR $comment == "") {
+			$err_message = "You must specify a value for name, email, and comment.";
+			return $err_message;
+		}
+		
+		if($_POST["address"] != "") {
+				$err_message = "Your form submission has an error.";
+				return $err_message;
+		}		
+	}
+	
+	
+}
+
 
 /* single guest display function, binds to entry 'id' unique variable
  */
