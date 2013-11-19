c4f29aff57516e758b6d9f4876c7f5d6c846c841
Michael Davidson <mikeyed101@gmail.com>
2013-11-19 12:29:26 -0500

    Update guest_record.php

--- inc/guest_record.php
+++ inc/guest_record.php
@@ -1,5 +1,55 @@
 <?php 
  
+  /* start of Guest as class */
+ class Guest {
+	public $name;
+	public $email;
+	public $comment;
+	 
+	public function add($name, $email, $comment) {
+		require("inc/guest_dbconnect.php");
+		
+		$entry_state = "";
+		
+		try { 
+			//State table and variables being inserted
+			$stmt = $db->prepare("INSERT INTO GuestBook (name, email, comment) VALUES (:name, :email, :comment)");
+			//Bind those variables inserted with the ones posted 
+			$stmt->bindParam(':name', $name);
+			$stmt->bindParam(':email', $email);
+			$stmt->bindParam(':comment', $comment);
+			//Execute the prepared statement
+			$stmt->execute();
+			
+			$entry_state = "Thank you for signing our guest book.";
+			return $entry_state;
+			
+		} catch (Exception $e) {
+				$entry_state = "Something happened. We failed to add your entry.";
+				return $entry_state;
+				exit;	
+		}
+	}
+	
+	/* single guest display function, binds to entry 'id' unique variable */
+	function get_single($entry_id) {
+		require("inc/guest_dbconnect.php");
+		
+		try { 
+			$result = $db->prepare("SELECT id, name, email, comment FROM GuestBook WHERE id = :id");
+			$result->bindParam(':id',$entry_id);
+			$result->execute();
+		} catch (Exception $e) {
+				echo "We are having trouble displaying this guest's information.";
+				exit;	
+		}
+
+		$entry = $result->fetch(PDO::FETCH_ASSOC);
+			
+		return $entry;
+	}
+}
+ 
 /* formats text for entry display */
 function get_list_view_html($entry) {
 	$output = "";
@@ -13,81 +63,6 @@ function get_list_view_html($entry) {
  	return $output;
 }
 
-
-function clean_val($value) {
-	$value_clean = trim($value);
-	$value_clean = stripslashes($value_clean);
-	$value_clean = htmlspecialchars($value_clean);
-	
-	return $value_clean;
-}
-	
-/* add a guest entry to the database */
-function add_entry($name, $email, $comment) {
-	require("inc/guest_dbconnect.php");
-	
-	try { 
-		//State table and variables being inserted
-		$stmt = $db->prepare("INSERT INTO GuestBook (name, email, comment) VALUES (:name, :email, :comment)");
-		//Bind those variables inserted with the ones posted 
-		$stmt->bindParam(':name', $name);
-		$stmt->bindParam(':email', $email);
-		$stmt->bindParam(':comment', $comment);
-		//Execute the prepared statement
-		$stmt->execute();
-	} catch (Exception $e) {
-			echo "Failed to add entry.";
-			exit;	
-	}
-}
-
-function validate_entry($name, $email, $comment) {
-	// define variables and set to empty values
-	$name = $email = $comment = "";
-			
-
-	/* reviews form submission for errors, omissions, or bots */
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		
-		$name =  trim($_POST["name"]);
-		$email = trim($_POST["email"]);
-		$comment = trim($_POST["comment"]);
-		
-		if($name == "" OR $email == "" OR $comment == "") {
-			$err_message = "You must specify a value for name, email, and comment.";
-			return $err_message;
-		}
-		
-		if($_POST["address"] != "") {
-				$err_message = "Your form submission has an error.";
-				return $err_message;
-		}		
-	}
-	
-	
-}
-
-
-/* single guest display function, binds to entry 'id' unique variable
- */
-function get_guest_single($entry_id) {
-	require("inc/guest_dbconnect.php");
-	
-	try { 
-		$result = $db->prepare("SELECT id, name, email, comment FROM GuestBook WHERE id = :id");
-		$result->bindParam(':id',$entry_id);
-		$result->execute();
-	} catch (Exception $e) {
-			echo "We are having trouble displaying this guest's information.";
-			exit;	
-	}
-
-	$product = $result->fetch(PDO::FETCH_ASSOC);
-		
-	return $product;
-}
- 
- 
 /* Grabs entries from database as 'entries' variable */
 function get_all_entries() {
 /*
@@ -124,7 +99,40 @@ function get_all_entries() {
 	$entries = $results->fetchAll(PDO::FETCH_ASSOC);
 	
 	return $entries;
-
 }
+
+/* cleans form submission values */
+function clean_val($value) {
+	$value_clean = trim($value);
+	$value_clean = stripslashes($value_clean);
+	$value_clean = htmlspecialchars($value_clean);
+	
+	return $value_clean;
+}
+
+function validate_entry($name, $email, $comment) {
+	// define variables and set to empty values
+	$name = $email = $comment = "";
+			
+
+	/* reviews form submission for errors, omissions, or bots */
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		$name =  trim($_POST["name"]);
+		$email = trim($_POST["email"]);
+		$comment = trim($_POST["comment"]);
+		
+		if($name == "" OR $email == "" OR $comment == "") {
+			$err_message = "You must specify a value for name, email, and comment.";
+			return $err_message;
+		}
+		
+		if($_POST["address"] != "") {
+				$err_message = "Your form submission has an error.";
+				return $err_message;
+		}		
+	}
+} 
+ 
 
 ?>
